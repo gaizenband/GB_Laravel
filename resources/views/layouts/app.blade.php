47cb6c451d845708be617e57605a1b36271b2866
gaizenband <gazik211@gmail.com>
2021-03-21 22:03:26 +0300

    added avatar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -69,6 +69,8 @@
                                 </li>
                             @endif
                         @else
+                            <img src="{{ Auth::User()->avatar }}" width="40" alt="">
+
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
